8fe98a11429f4c3a7c9fec5473a5d6ae0298c21b
Windows81 <nastyahmede@gmail.com>
2023-04-25 07:10:05 +0000

    2023-10-25 07:04:05 UTC

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<p align="center">As of 2023-08-04/25/23 07:04:47 UTC</p>
+<p align="center">As of 2023-10-25 07:04:05 UTC</p>
 <h1 align="center">VisualPlugin is not yet in the Byfron experiment.</h1>
----
 He is still enrolled in the *LIVE* channel, or Hyperion might've already seeped into other live channels.
--- check.py
+++ check.py
@@ -13,16 +13,14 @@ if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     with open('README.md', 'w') as f:
         r = check()
-        d = datetime.datetime.now().strftime('%Y-%M-%D %H:%m:%S UTC')
+        d = datetime.datetime.now().strftime('%Y-%M-%d %H:%m:%S UTC')
         f.write('\r\n'.join([
             f"""<p align="center">As of {d}</p>""",
         ] + (r.lower() != 'zwinplayer64' and [
             f"""<h1 align="center">VisualPlugin is not yet in the Byfron experiment.</h1>""",
-            f"""---""",
             f"""He is still enrolled in the *{check()}* channel, or Hyperion might've already seeped into other live channels."""
         ] or [
             f"""<h1 align="center">VisualPlugin has been enrolled into the hellscape that is Byfron and its trumper anti-tamper antics!</h1>""",
-            f"""---""",
             f"""Congrats!  I am no longer able to run Rsexec, *but* there are tools out there which could help mitigate the problem until Hyperion is fully released.  I used Requestly to force me to remain on the *LIVE* channel (script is in this repository), so I should be safe for now, as of 2023-04-25.  There are other solutions which work for TamperMonkey."""
         ])))
 
